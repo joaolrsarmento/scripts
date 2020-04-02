465510d84171f4450f932fbe66fce6cbd752fc05
João Sarmento <joaolrsarmento@gmail.com>
2020-04-02 12:40:31 -0300

    Connection

---
+++ email/pizzaria/constants.py
@@ -0,0 +1,8 @@
+# Pop gmail host
+GMAIL_HOST = 'pop.gmail.com'
+# Pop gmail port
+GMAIL_PORT = 995
+# Enable less secure apps url
+ENABLE_LESS_SECURE_APPS_URL = 'https://myaccount.google.com/lesssecureapps'
+# Save directory
+SAVE_DIR = 'tmp/'
